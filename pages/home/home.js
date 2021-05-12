0aa697872134b5536567f51bffd2d337e3af3c00
ZM <415741492@qq.com>
2021-05-12 22:15:09 +0800

    事件相关

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,62 @@
 // page/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    titles: ["a", "b", "c"]
+  },
+  handleBtnClick: function () {
+    console.log(1);
+  },
+  handleTouchStart: function (evt) {
+    console.log(evt.type);
+    console.log("点击开始时触发");
+  },
+  handleTouchmove: function () {
+    console.log("点击移动时触发");
+  },
+  handleTouchEnd: function () {
+    console.log("点击结束时触发");
+  },
+  handleTap: function () {
+    console.log("点击时触发");
+  },
+  handleLongPress: function () {
+    console.log("长按超过350s时触发，如果指定了事件回调函数并且触发了，tap事件不会被触发");
+  },
+  handleClickEvent: function (evt) {
+    console.log(evt);
+  },
+  handleTouchEndEvent: function (event) {
+    console.log(event);
+  },
+  handleInner: function (event) {
+    // console.log(event);
+  },
+  handleOuter: function (event) {
+    //点击inner时，event中的target和currentTarget不同，tatget是inner，currentTarget是outer
+    console.log(event);
+  },
+  handleItemClick: function (event) {
+    console.log(event);
+    console.log(event.currentTarget.dataset);
+  },
+  //事件冒泡和事件捕获
+  handleCaptureView1: function () {
+    console.log("handleCaptureView1");
+  },
+  handleCaptureView2: function () {
+    console.log("handleCaptureView2");
+  },
+  handleCaptureView3: function () {
+    console.log("handleCaptureView3");
+  },
+  handleBindView1: function () {
+    console.log("handleBindView1");
+  },
+  handleBindView2: function () {
+    console.log("handleBindView2");
+  },
+  handleBindView3: function () {
+    console.log("handleBindView3");
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
