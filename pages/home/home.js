4d70bbc2b5d0a401caef9bccc466b222ea46dc6c
ZM <415741492@qq.com>
2021-05-30 21:21:37 +0800

    网络请求及封装

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,5 @@
 // page/home.js
+import request from "../../service/network.js";
 Page({
 
   /**
@@ -12,55 +13,48 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //原生发送网络请求
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/recommend',
+    //   success: function (result) {
+    //     console.log(result);
+    //   }
+    // })
+    // wx.request({
+    //   url: 'http://152.136.185.210:7878/api/m5/home/data',
+    //   data: {
+    //     type: "sell",
+    //     page: 1
+    //   },
+    //   success: function (result) {
+    //     console.log(result);
+    //   }
+    // })
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   method: "POST",
+    //   data: {
+    //     name: "haha",
+    //     age: "18"
+    //   },
+    //   success: function (res) {
+    //     console.log(res);
+    //   }
+    // })
 
+    //封装后
+    request({
+      url: "http://httpbin.org/post",
+      method: "POST",
+      data: {
+        name: "haha",
+        age: "18"
+      },
+    }).then(res => {
+      console.log(res);
+    }).catch(err => {
+      console.log(err);
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
 })
